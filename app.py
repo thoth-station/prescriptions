21341471c9907cc0a7449db2a50423bef2fb9836
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2021-06-16 08:23:05 +0200

    Let S2I tooling inject version information

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright(C) 2021 Project Thoth
+
+__version__ = "0.0.0"
+
+
+def main():
+    raise NotImplementedError("This application was not meant to be directly invoked")
+
+
+__name__ == "__main__" and main()
